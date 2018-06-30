c7a39b55fe65d0e1fec83b474d19c08fad732b70
Lúcio Ferreira da Silva <sandmanlucio@gmail.com>
2018-06-30 00:32:44 -0300

    Commit inicial dos arquivos do projeto.

---
+++ src/main/java/br/com/lambadadigital/osantosodia/api/OsantododiaApplication.java
@@ -0,0 +1,12 @@
+package br.com.lambadadigital.osantosodia.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsantododiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsantododiaApplication.class, args);
+	}
+}
